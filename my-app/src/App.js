b358b72b8cbee075565919fa37077e3c761705f5
kunalharmalkar <kunal.s.harmalkar@gmail.com>
2023-07-05 12:26:38 -0400

    added last name

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         hi sophie! - Kunal
+         hi sophie! - Kunal Harmalkar
         </p>
         <a
           className="App-link"
